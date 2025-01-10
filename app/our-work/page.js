79c8bb215e733f84adac77f22d2a6913334674e4
Owais Zarger <owaiszarger@Owais-Macbook-Air.local>
2025-01-10 11:06:48 +0530

    latest develop pull

--- app/our-work/page.js
+++ app/our-work/page.js
@@ -1,7 +1,6 @@
 import ImageGrid from "@/components/common-image-grid";
 import { Footer } from "@/components/footer/footer";
 import HeroSection from "@/components/our-work/hero-section";
-import { data1, data2, data3 } from "@/components/our-work/image-data";
 import ProjectsFilter from "@/components/our-work/projects-filter";
 
 export default function TechSolutions() {
