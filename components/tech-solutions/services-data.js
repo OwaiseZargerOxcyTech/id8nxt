3b7f5d4ffac4741d3fe0e461d63a401beda747cc
ZahidKhanOxytech <zahid.khan@oxcytech.com>
2025-01-10 16:35:00 +0530

    removed mleft field from service-data array of objects

--- components/tech-solutions/services-data.js
+++ components/tech-solutions/services-data.js
@@ -2,42 +2,34 @@ export const services_data = [
   {
     name: "Custom Web Development",
     description: "Building custom websites tailored to your needs.",
-    mleft: "165px",
   },
   {
     name: "Web Personalization",
     description: "Making the web personal, one user at a time.",
-    mleft: "170px",
   },
   {
     name: "UI/UX",
     description: "Creating intuitive and engaging user experiences.",
-    mleft: "165px",
   },
   {
     name: "SEO",
     description: "Optimizing your website for search engine visibility.",
-    mleft: "165px",
   },
   {
     name: "CRM & ERP",
     description:
       "Streamlining business processes with custom CRM and ERP solutions.",
-    mleft: "165px",
   },
   {
     name: "E-Commerce",
     description: "Developing robust and scalable e-commerce platforms.",
-    mleft: "160px",
   },
   {
     name: "Email Marketing",
     description: "Driving engagement with targeted email campaigns.",
-    mleft: "160px",
   },
   {
     name: "Marketing Automation",
     description: "Automating repetitive marketing tasks for efficiency.",
-    mleft: "160px",
   },
 ];
