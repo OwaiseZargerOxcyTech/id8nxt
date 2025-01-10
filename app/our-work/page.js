2275f5cbccbc221f269b2244366a58be5154a21b
Owais Zarger <owaiszarger@Owais-Macbook-Air.local>
2025-01-10 09:43:29 +0530

    refactor image grid complete page

--- app/our-work/page.js
+++ app/our-work/page.js
@@ -1,8 +1,7 @@
+import ImageGrid from "@/components/common-image-grid";
 import { Footer } from "@/components/footer/footer";
 import HeroSection from "@/components/our-work/hero-section";
-import PortfolioGrid1 from "@/components/our-work/portfolio-grid-1";
-import PortfolioGrid2 from "@/components/our-work/portfolio-grid-2";
-import PortfolioGrid3 from "@/components/our-work/portfolio-grid-3";
+import { data1, data2, data3 } from "@/components/our-work/image-data";
 import ProjectsFilter from "@/components/our-work/projects-filter";
 
 export default function TechSolutions() {
@@ -10,9 +9,9 @@ export default function TechSolutions() {
     <main>
       <HeroSection />
       <ProjectsFilter />
-      <PortfolioGrid1 />
-      <PortfolioGrid2 />
-      <PortfolioGrid3 />
+      <ImageGrid data={data1} />
+      <ImageGrid data={data2} />
+      <ImageGrid data={data3} />
       <Footer color={"white"} textColor={"black"} />
     </main>
   );
