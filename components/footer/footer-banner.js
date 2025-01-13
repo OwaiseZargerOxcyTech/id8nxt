887a21d056253375867341b24ad687547f4acadb
Owais Zarger <owaiszarger@Owais-Macbook-Air.local>
2025-01-13 17:59:31 +0530

    client product and footer-banner fix

--- components/footer/footer-banner.js
+++ components/footer/footer-banner.js
@@ -3,7 +3,7 @@ export function FooterBanner({
   textColor
 }) {
   return (
-    <div className="relative mx-4 mt-24 mb-12 pb-[2vw] group cursor-pointer flex items-center justify-center">
+    <div className="relative mx-4 mt-24 mb-12 group cursor-pointer flex items-center justify-center">
       {/* Sliding background */}
       <div className="absolute inset-0 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-700 ease-in-out origin-left"></div>
 
