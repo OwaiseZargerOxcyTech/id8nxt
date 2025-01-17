94ed857b40e8ca6180d52d7defac4aa8fdd074ec
Owais Zarger <owaiszarger@Owais-Macbook-Air.local>
2025-01-17 17:41:18 +0530

    new hero about

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,4 @@
 import HeroAbout from "@/components/about/hero";
-import HeroSection from "@/components/about/new";
 import OurTeam from "@/components/about/our-team";
 import WhoWeAre from "@/components/about/whoweare";
 import { ContactForm } from "@/components/footer/contact-form";
@@ -8,7 +7,6 @@ import { Footer } from "@/components/footer/footer";
 export default function AboutPage() {
   return (
     <main>
-      {/* <HeroSection /> */}
       <HeroAbout />
       <WhoWeAre />
       <OurTeam />
