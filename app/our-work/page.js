ba1eae7206853c76a2c1259154366150d78e8ef3
Owais Zarger <owaiszarger@Owais-Macbook-Air.local>
2025-01-20 19:15:57 +0530

    remove old hero

--- app/our-work/page.js
+++ app/our-work/page.js
@@ -1,13 +1,11 @@
 import { Footer } from "@/components/footer/footer";
 import ParallaxHero from "@/components/our-work/hero";
-import HeroSection from "@/components/our-work/hero-section";
 import ProjectsFilter from "@/components/our-work/projects-filter";
 
 export default function OurWork() {
   return (
     <main>
       <ParallaxHero />
-      {/* <HeroSection /> */}
       <ProjectsFilter />
       <Footer color={"white"} textColor={"black"} />
     </main>
