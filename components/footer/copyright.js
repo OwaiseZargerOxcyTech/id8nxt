2d29d63155110ee83ead5e4a5e963eb7ee9790f8
Owais Zarger <owaiszarger@Owais-Macbook-Air.local>
2025-01-24 00:19:44 +0530

    fix copyright padding x - 16

--- components/footer/copyright.js
+++ components/footer/copyright.js
@@ -1,7 +1,7 @@
 export function Copyright({ textColor }) {
   return (
     <main className="w-full">
-      <div className="xl:max-w-6xl 2xl:max-w-screen-xl 3xl:max-w-screen-2xl 4xl:max-w-screen-4xl mx-auto px-4 sm:px-6 lg:px-0 flex justify-start items-center ">
+      <div className="xl:max-w-6xl 2xl:max-w-screen-xl 3xl:max-w-screen-2xl 4xl:max-w-screen-4xl mx-auto px-4 sm:px-6 lg:px-16 flex justify-start items-center ">
         <p className="text-sm text-white/80" style={{ color: textColor }}>
           Developed by ID8NXT - Copyright © 2024 All Rights Reserved.
         </p>
