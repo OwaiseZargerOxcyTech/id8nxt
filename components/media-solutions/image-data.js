f9b548da5a59504e9809fe4e52f265ed49074160
Owais Zarger <owaiszarger@Owais-Macbook-Air.local>
2025-02-03 15:12:07 +0530

    attach links in tech, media and brand solutions

--- components/media-solutions/image-data.js
+++ components/media-solutions/image-data.js
@@ -3,30 +3,36 @@ export const media_data = [
     id: 1,
     image: "/images/our-work/jonaki.jpg",
     title: "Jonaki _ Perfume",
+    link: "/case-study/jonaki",
   },
   {
     id: 2,
     image: "/images/our-work/me&twa.png",
     title: "Me and TVA _ Children's brand",
+    link: "/case-study/me-and-tva",
   },
   {
     id: 3,
     image: "/images/our-work/nutting.png",
-    title: "Jonki _ Perfume",
+    title: "Nutting _ Like _ it",
+    link: "/case-study/nutting-like-it",
   },
   {
     id: 4,
     image: "/images/our-work/robo_6-615x665.png",
     title: "Good Little Robots _ Logo Design | Brand Visualization",
+    link: "/case-study/glr",
   },
   {
     id: 5,
     image: "/images/our-work/appo_cover-3.png",
     title: "Appo _ Dynamic salon",
+    link: "/case-study/appo",
   },
   {
     id: 6,
     image: "/images/our-work/rrtboard-1-copy-29.png",
     title: "Flightronics _ Perfume",
+    link: "/case-study/flightroniks",
   },
 ];
