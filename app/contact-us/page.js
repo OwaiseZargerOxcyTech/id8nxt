c467d2fc5473619d199f2ce75e94beae3b06918f
Owais Zarger <owaiszarger@Owais-Macbook-Air.local>
2025-02-11 10:33:51 +0530

    contatc hero animation

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -1,13 +1,11 @@
 import ContactForm from "@/components/contact-us/contact-form";
 import ContactSection from "@/components/contact-us/contact-section";
 import Hero from "@/components/contact-us/hero";
-import HeroSection from "@/components/contact-us/hero-section";
 import { Footer } from "@/components/footer/footer";
 
 export default function Contact() {
   return (
     <main>
-      {/* <HeroSection /> */}
       <Hero />
       <ContactSection />
       <ContactForm />
